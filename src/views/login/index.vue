d7648bd97bf3a8e841d551071bb1e03b4e68ec39
Jux1nCc <619426275@qq.com>
2020-06-30 14:49:36 +0800

    输入框验证完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,12 @@
   <div class="login">
     <van-nav-bar title="登录" />
     <van-cell-group>
-      <van-field left-icon="smile-o" v-model="obj.phone" placeholder="请输入手机号">
+      <van-field left-icon="smile-o" required :error-message="result.phone" v-model="obj.phone" placeholder="请输入手机号">
         <template #left-icon>
           <i class="iconfont icon-shouji"></i>
         </template>
       </van-field>
-      <van-field left-icon="smile-o" v-model="obj.code" placeholder="请输入验证码">
+      <van-field left-icon="smile-o" required :error-message="result.code" v-model="obj.code" placeholder="请输入验证码">
         <template #button>
           <van-button size="small" color="#ededed" round class="codeBtn" >发送验证码</van-button>
         </template>
@@ -17,7 +17,7 @@
       </van-field>
     </van-cell-group>
     <div class="loginBtn">
-      <van-button type="info" size="large">登录</van-button>
+      <van-button type="info" size="large" @click="login">登录</van-button>
     </div>
   </div>
 </template>
@@ -27,8 +27,45 @@ export default {
   data () {
     return {
       obj: {
+        phone: '18888888888',
+        code: '246810'
+      },
+      result: {
         phone: '',
         code: ''
+      }
+    }
+  },
+  methods: {
+    // 登录点击事件
+    login () {
+      if (this.checkData() === false) {
+        return
+      }
+      console.log('验证成功')
+    },
+    // 验证方法
+    checkData () {
+      // 是否通过验证
+      var isPass = []
+      if (this.obj.phone.trim().length === 11) {
+        isPass.push(true)
+        this.result.phone = ''
+      } else {
+        isPass.push(false)
+        this.result.phone = '输入正确的手机号'
+      }
+      if (this.obj.code.trim().length === 6) {
+        isPass.push(true)
+        this.result.code = ''
+      } else {
+        isPass.push(false)
+        this.result.code = '请输入正确的验证码'
+      }
+      if (isPass.includes(false)) {
+        return false
+      } else {
+        return true
       }
     }
   }
