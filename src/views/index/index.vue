74e724a02456595ef9f54d90caea4942c553d137
Jux1nCc <619426275@qq.com>
2020-06-30 21:04:24 +0800

    下拉刷新完成

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,14 +3,16 @@
     <van-nav-bar :fixed="true" title="首页" />
     <van-tabs v-model="active">
       <van-tab :title='"标签" + item' v-for="item in 8" :key="item">
-        <van-list
-          v-model="loading"
-          :finished="finished"
-          finished-text="-- 我是有底线的 --"
-          @load="onLoad"
-        >
-          <van-cell v-for="item in list" :key="item" :title="item" />
-        </van-list>
+        <van-pull-refresh v-model="isLoading" :head-height="80" @refresh="onRefresh" success-text="刷新成功">
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="-- 我是有底线的 --"
+            @load="onLoad"
+          >
+            <van-cell v-for="item in list" :key="item" :title="item" />
+          </van-list>
+        </van-pull-refresh>
       </van-tab>
     </van-tabs>
     <div class="channelBtn">
@@ -30,7 +32,9 @@ export default {
       // list组件中的数据源是否加载完毕
       finished: false,
       // 文章数据源
-      list: []
+      list: [],
+      // 下拉值的状态
+      isLoading: true
     }
   },
   methods: {
@@ -40,11 +44,19 @@ export default {
         var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         this.list.push(...arr)
         this.loading = false
-        if (this.list.length === 60) {
+        if (this.list.length >= 60) {
           this.finished = true
         }
         console.log('onLoad')
         console.log(this.list.length)
+      }, 1000)
+    },
+    onRefresh () {
+      setTimeout(() => {
+        console.log('onRefresh')
+        this.list = []
+        this.onLoad()
+        this.isLoading = false
       }, 1000)
     }
   }
