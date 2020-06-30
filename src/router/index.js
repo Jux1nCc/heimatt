13a5f0b55d2bfe391eb09b94e05e2e5fa6eee345
Jux1nCc <619426275@qq.com>
2020-06-30 18:31:33 +0800

    完成home页子组件index和my

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 导入useVant
-import UseVant from '@/views/useVant/index.vue'
+import UseVant from '@/views/useVant/index'
 // 导入login
-import Login from '@/views/login/index.vue'
+import Login from '@/views/login/index'
+// 导入home
+import Home from '@/views/home/index'
+// 导入index
+import Index from '@/views/index/index'
+// 导入my
+import My from '@/views/my/my'
 
 Vue.use(VueRouter)
 
@@ -12,9 +18,25 @@ const routes = [
     path: '/usevant',
     component: UseVant
   },
+  // 登录页
   {
     path: '/login',
     component: Login
+  },
+  // 首页
+  {
+    path: '/home',
+    component: Home,
+    children: [
+      {
+        path: '/index',
+        component: Index
+      },
+      {
+        path: '/my',
+        component: My
+      }
+    ]
   }
 ]
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <van-nav-bar />
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/index" icon="home-o">主页</van-tabbar-item>
+      <van-tabbar-item icon="search">问答</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/my" icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.van-nav-bar.van-hairline--bottom {
+  background: #3296fa;
+}
+/deep/.van-nav-bar__title.van-ellipsis {
+  color: #fff;
+}
+
+</style>
