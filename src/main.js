477ffad6980767b7500b0afb307cdc57f6a04c3d
Jux1nCc <619426275@qq.com>
2020-06-30 10:32:58 +0800

    vant的基本使用

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
+// 导入vue
 import Vue from 'vue'
+// 导入App
 import App from './App.vue'
+// 导入路由
 import router from './router'
+// 导入vuex
 import store from './store'
+// 导入vant
+import Vant from 'vant'
+// 导入vant样式
+import 'vant/lib/index.css'
+
+// 使用vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
+// 创建Vue实例
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 导入useVant
+import UseVant from '@/views/useVant/index.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/usevant',
+    component: UseVant
+  }
+]
 
 const router = new VueRouter({
   routes
